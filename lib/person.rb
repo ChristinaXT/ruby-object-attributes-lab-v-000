46250bc09d6e6022a46a7995a7eafb3a8fb9e5bb
Christina Williams <christinaxtwilliams@gmail.com>
2018-09-13 17:06:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,19 @@
-* class Person
-*  
-*   def initialize(name, job)
-*     @name = name
-*     @job = job
-*   end
-*  
-*   def name=(full_name)
-*     first_name, last_name = full_name.split
-*     @first_name = first_name
-*     @last_name = last_name
-*   end
-*  
-*   def name
-*     "#{@first_name} #{@last_name}".strip
-*   end
-*  
-* end
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
+end
